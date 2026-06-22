c47b2c3c5cbe9dee832a29e17c94a9f244f3a1be
Jatin Mittal <mittaljatin2004@gmail.com>
2026-06-22 13:26:23 +0530

    Fix: Add fileReplacements and production environment file

---
+++ src/frontend/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://claimcore-api-jatin2026-a5b9eqbdcvavabfh.centralindia-01.azurewebsites.net/api',
+  uploadBaseUrl: 'https://claimcore-api-jatin2026-a5b9eqbdcvavabfh.centralindia-01.azurewebsites.net'
+};
